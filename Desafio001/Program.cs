9d51f27ffb6de9993459ad37339822a5c66cfb66
leandroctorres <leandrocesariotorres@gmail.com>
2024-01-05 14:09:37 -0300

    commit inicial

---
+++ Desafio001/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Desafio: Aula 2");
+
+int notaMedia = 6;
+
+if (notaMedia >= 5)
+{
+    Console.WriteLine("Nota suficiente para aprovação");
+}
+else
+{
+    Console.WriteLine("Nota insuficiente para aprovação");
+}
